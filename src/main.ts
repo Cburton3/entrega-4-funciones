178e936a62914c07c3e0652b1c50942e6e00c988
Charles Burton <charlie-246@hotmail.co.uk>
2023-08-11 15:54:32 +0200

    written in typescript

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,86 @@
-
-const element = document.querySelector(".numero-turno");
-let currentNumber = parseInt(element?.innerText);
-const text = document.querySelector(".texto-turno");
+// const enterButton = document.getElementById("enter");
+// if (
+//   enterButton !== null &&
+//   enterButton !== undefined &&
+//   enterButton instanceof HTMLButtonElement
+// ) {
+//   enterButton.addEventListener("click", enter);
+// }
+// function enter() {}
 
 function nextNumber(): void {
-  currentNumber = ++currentNumber;
+  const element = document.querySelector(".numero-turno");
   if (
     element !== null &&
     element !== undefined &&
-    element instanceof HTMLHeadingElement &&
-    text !== null &&
-    text !== undefined &&
-    text instanceof HTMLElement
+    element instanceof HTMLHeadingElement
   ) {
-    element.innerText = `${currentNumber}`.padStart(2, "0");
-    text.innerText = "Por favor, acérquese al mostrador";
+    let currentNumber = parseInt(element.innerText);
+    element.innerText = `${++currentNumber}`.padStart(2, "0");
+    // element.innerText = "Por favor, acérquese al mostrador";
   }
 }
 
 function previousNumber(): void {
-  if (currentNumber > 0) {
-    if (
-      element !== null &&
-      element !== undefined &&
-      element instanceof HTMLHeadingElement &&
-      text !== null &&
-      text !== undefined &&
-      text instanceof HTMLElement
-    ) {
+  const element = document.querySelector(".numero-turno");
+  if (
+    element !== null &&
+    element !== undefined &&
+    element instanceof HTMLHeadingElement
+  ) {
+    let currentNumber = parseInt(element.innerText);
+    if (currentNumber > 0) {
       currentNumber = --currentNumber;
       element.innerText = `${currentNumber}`.padStart(2, "0");
-      text.innerText = "Ya siendo atentido";
+      const text = document.querySelector(".texto-turno");
+      if (text !== null && text !== undefined && text instanceof HTMLElement) {
+        text.innerText = "Ya siendo atentido";
+      }
     }
   }
 }
 
-function resetNumber() : void {
-  currentNumber = 0;
+function resetNumber(): void {
+  const element = document.querySelector(".numero-turno");
   if (
     element !== null &&
     element !== undefined &&
-    element instanceof HTMLHeadingElement &&
-    text !== null &&
-    text !== undefined &&
-    text instanceof HTMLElement
+    element instanceof HTMLHeadingElement
   ) {
-    element.innerText = `${currentNumber}`.padStart(2, "0");
-    text.innerText = "Espere a ser atendido";
+    let currentNumber = parseInt(element.innerText);
+    currentNumber = 0;
+
+    {
+      element.innerText = `${currentNumber}`.padStart(2, "0");
+      const text = document.querySelector(".texto-turno");
+      if (text !== null && text !== undefined && text instanceof HTMLElement) {
+        text.innerText = "Espere a ser atendido";
+      }
+    }
   }
 }
-
 const nextButton = document.getElementById("next");
-const backButton = document.getElementById("back");
-const resetButton = document.getElementById("reset");
+if (
+  nextButton !== null &&
+  nextButton !== undefined &&
+  nextButton instanceof HTMLHeadingElement
+) {
+  nextButton.addEventListener("click", () => nextNumber());
+}
 
-nextButton?.addEventListener("click", () => nextNumber());
-backButton?.addEventListener("click", () => previousNumber());
-resetButton?.addEventListener("click", () => resetNumber());
+const backButton = document.getElementById("back");
+if (
+  backButton !== null &&
+  backButton !== undefined &&
+  backButton instanceof HTMLHeadingElement
+) {
+  backButton.addEventListener("click", () => previousNumber());
+}
+const resetButton = document.getElementById("reset");
+if (
+  resetButton !== null &&
+  resetButton !== undefined &&
+  resetButton instanceof HTMLHeadingElement
+) {
+  resetButton.addEventListener("click", () => resetNumber());
+}
