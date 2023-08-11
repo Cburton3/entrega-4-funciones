0175339946ef2e8db30f5c0001921a01023b1f6b
Charles Burton <charlie-246@hotmail.co.uk>
2023-08-11 18:13:05 +0200

    input button

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
-// const enterButton = document.getElementById("enter");
-// if (
-//   enterButton !== null &&
-//   enterButton !== undefined &&
-//   enterButton instanceof HTMLButtonElement
-// ) {
-//   enterButton.addEventListener("click", enter);
-// }
-// function enter() {}
+
+function enter() : void {
+  const element = document.querySelector(".numero-turno");
+  const number = document.getElementById('input-number') as HTMLInputElement;
+  if (
+    element !== null &&
+    element !== undefined &&
+    element instanceof HTMLHeadingElement &&
+    number !== null &&
+    number !== undefined &&
+    number instanceof HTMLElement
+  ) {
+    element.innerText = number.value;
+    // let currentNumber = parseInt(element.innerText);
+    // element.innerText = `${currentNumber}`.padStart(2, "0");
+    // currentNumber = number.value;
+  }
+}
+
+const enterButton = document.getElementById("enter");
+if (
+  enterButton !== null &&
+  enterButton !== undefined &&
+  enterButton instanceof HTMLButtonElement
+) {
+  enterButton.addEventListener("click", enter);
+}
 
 function nextNumber(): void {
   const element = document.querySelector(".numero-turno");
@@ -84,3 +102,4 @@ if (
 ) {
   resetButton.addEventListener("click", () => resetNumber());
 }
+
