c499850979123e0aef4843954ca33de2eb32b70e
Charles Burton <charlie-246@hotmail.co.uk>
2023-08-29 14:02:26 +0200

    final commit

--- src/main.js
+++ src/main.js
@@ -1,3 +1,39 @@
-import "./style.css";
+const element = document.querySelector(".numero-turno");
+let currentNumber = parseInt(element.innerText);
+const text = document.querySelector(".texto-turno");
+const number = document.getElementById('input-number');
 
-console.log("Hello from main");
+function nextNumber() {
+  currentNumber = ++currentNumber;
+  element.innerText = `${currentNumber}`.padStart(2, "0");
+  text.innerText = 'Por favor, acérquese al mostrador';
+
+}
+
+function previousNumber() {
+  if (currentNumber > 0) {
+    currentNumber = --currentNumber;
+    element.innerText = `${currentNumber}`.padStart(2, "0");
+    text.innerText = 'Ya siendo atentido';
+  }
+}
+
+function resetNumber() {
+  currentNumber = 0;
+  element.innerText = `${currentNumber}`.padStart(2, "0");
+  text.innerText = 'Espere a ser atendido';
+}
+
+function enterNumber() {
+  element.textContent = `${number.value}`.padStart(2, "0");
+}
+
+const nextButton = document.getElementById("next");
+const backButton = document.getElementById("back");
+const resetButton = document.getElementById("reset");
+const enterButton = document.getElementById("enter");
+
+nextButton.addEventListener("click", () => nextNumber());
+backButton.addEventListener("click", () => previousNumber());
+resetButton.addEventListener("click", () => resetNumber());
+enterButton.addEventListener("click", () => enterNumber());
