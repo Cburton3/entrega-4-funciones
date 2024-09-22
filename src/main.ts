97e94c0e76dd92e31b5f1cb845876e919a2b67b1
Charles <101049827+Cburton3@users.noreply.github.com>
2024-09-22 16:10:13 +0200

    revising and changing fx names for readability

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-
-function enter() : void {
-  const element = document.querySelector(".numero-turno");
-  const number = document.getElementById('input-number') as HTMLInputElement;
-  const text = document.querySelector(".texto-turno");
+function enter(): void {
+  const element = document.querySelector(".turn-number");
+  const number = document.getElementById("input-number") as HTMLInputElement;
+  const text = document.querySelector(".turn-text");
 
   if (
     element !== null &&
@@ -12,45 +11,46 @@ function enter() : void {
     number !== undefined &&
     number instanceof HTMLElement
   ) {
-    element.textContent = number.value;
-    let currentNumber = parseInt(element.innerText);
+    element.textContent = number.value; //here its still a string
+    let currentNumber = parseInt(element.innerText); //here its a number so we can use parseInt
     element.innerText = `${currentNumber}`.padStart(2, "0");
-  } else if (text !== null && text !== undefined && text instanceof HTMLElement) {
-    text.textContent = "Por favor, pon un número";
+  } else if (
+    text !== null &&
+    text !== undefined &&
+    text instanceof HTMLElement
+  ) {
+    text.textContent = "Please introduce number";
   }
-   
 }
 
-const enterButton = document.getElementById("enter");
-if (
-  enterButton !== null &&
-  enterButton !== undefined &&
-  enterButton instanceof HTMLButtonElement
-) {
-  enterButton.addEventListener("click", enter);
-}
+//re text content  returns all the text within an element, including text in hidden elements
+// innerText respects the styles and layout of the page
 
 function nextNumber(): void {
-  const element = document.querySelector(".numero-turno");
-  const text = document.querySelector(".texto-turno");
+  const element = document.querySelector(".turn-number");
+  const text = document.querySelector(".turn-text");
   if (
     element !== null &&
     element !== undefined &&
     element instanceof HTMLHeadingElement &&
-    text !== null && 
-    text !== undefined && 
+    text !== null &&
+    text !== undefined &&
     text instanceof HTMLElement
   ) {
-    let currentNumber = parseInt(element.innerText); //inner text here just gets you the string text between teh two tags. the inner text part is just referncing it
-    //curretNumber is coverted to a number in order to run the ++ operation 
-    element.textContent = `${++currentNumber}`.padStart(2, "0"); //as using the .padstart you'd be referencing the currentNumber.padstart instead of 'currnetNumber and then applying the padstart to it.
+    let currentNumber = parseInt(element.innerText);
+    //parseInt converts a string into an integer
+    //inner text here just gets you the string text between teh two tags. the inner text part is just referncing it
+    //curretNumber is coverted to a number in order to run the ++ operation
+    element.textContent = `${++currentNumber}`.padStart(2, "0");
+    //pad start adding soemthing/padding/number (in this case) before the string (looks better)
+    //as using the .padstart you'd be referencing the currentNumber.padstart instead of 'currnetNumber and then applying the padstart to it.
     //We convert the number back to a stringinorder to run teh padstart op which needs a string
-    text.textContent = "Por favor, acérquese al mostrador";
+    text.textContent = "Please proceed to the front desk";
   }
 }
 
 function previousNumber(): void {
-  const element = document.querySelector(".numero-turno");
+  const element = document.querySelector(".turn-number");
   if (
     element !== null &&
     element !== undefined &&
@@ -60,16 +60,16 @@ function previousNumber(): void {
     if (currentNumber > 0) {
       currentNumber = --currentNumber;
       element.textContent = `${currentNumber}`.padStart(2, "0");
-      const text = document.querySelector(".texto-turno");
+      const text = document.querySelector(".turn-text");
       if (text !== null && text !== undefined && text instanceof HTMLElement) {
-        text.textContent = "Ya siendo atentido";
+        text.textContent = "Being served";
       }
     }
   }
 }
 
 function resetNumber(): void {
-  const element = document.querySelector(".numero-turno");
+  const element = document.querySelector(".turn-number");
   if (
     element !== null &&
     element !== undefined &&
@@ -80,13 +80,24 @@ function resetNumber(): void {
 
     {
       element.innerText = `${currentNumber}`.padStart(2, "0");
-      const text = document.querySelector(".texto-turno");
+      const text = document.querySelector(".turn-text");
       if (text !== null && text !== undefined && text instanceof HTMLElement) {
-        text.textContent = "Espere a ser atendido";
+        text.textContent = "Please wait to be served";
       }
     }
   }
 }
+//buttons
+
+const enterButton = document.getElementById("enter");
+if (
+  enterButton !== null &&
+  enterButton !== undefined &&
+  enterButton instanceof HTMLButtonElement
+) {
+  enterButton.addEventListener("click", enter);
+}
+
 const nextButton = document.getElementById("next");
 if (
   nextButton !== null &&
@@ -112,4 +123,3 @@ if (
 ) {
   resetButton.addEventListener("click", () => resetNumber());
 }
-
